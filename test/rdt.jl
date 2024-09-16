55c75c0e8c0e0b4755365f923bd0aeb1707bdf6f
David M Fobes <dfobes@lanl.gov>
2024-09-16 11:48:24 -0600

    UPD: github actions workflows

--- test/rdt.jl
+++ test/rdt.jl
@@ -3,6 +3,5 @@
     data = parse_file(small_data)
     result = solve_rdt(data, ACPUPowerModel, juniper_solver)
     @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == ALMOST_LOCALLY_SOLVED
-    @test result["termination_status"] == LOCALLY_SOLVED || result["termination_status"] == ALMOST_LOCALLY_SOLVED
     @test isapprox(result["objective"], 167, atol=1e-1)
 end
