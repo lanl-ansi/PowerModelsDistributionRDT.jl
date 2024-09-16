45d6442f40abb5f3682ecf627b76d97b56a04b01
David M Fobes <dfobes@lanl.gov>
2024-09-16 11:50:31 -0600

    UPD: Documenter.jl script

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,23 @@ using Documenter
 using PowerModelsDistributionRDT
 
 makedocs(
+    modules = [PowerModelsDistributionRDT],
+    format = Documenter.HTML(
+        analytics = "",
+        mathengine = Documenter.MathJax(),
+        prettyurls=false,
+        collapselevel=1,
+    ),
+    strict=false,
     sitename = "PowerModelsDistributionRDT",
-    format = Documenter.HTML(),
-    modules = [PowerModelsDistributionRDT]
+    authors = "Russell Bent, Jose Tabarez, David M Fobes, and contributors",
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/lanl-ansi/PowerModelsDistributionRDT.jl.git",
+    push_preview = false,
+    devbranch = "main",
+)
